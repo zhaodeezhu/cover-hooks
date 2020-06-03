2091f32f104898cb187bef59ffac0e6880afdbb0
赵秀全 <zhaoxiuquan@cvte.com>
2020-06-03 19:09:37 +0800

    [fix] 表格列合并测试

--- src/components/MyName.tsx
+++ src/components/MyName.tsx
@@ -13,19 +13,43 @@ const initColumns = [
     dataIndex: 'name',
     key: 'name',
     width: 200,
-    fixed: 'left'
+    fixed: 'left',
+    render: (text, record, index) => {
+      if(index === 0) {
+        return {
+          children: text,
+          props: {
+            rowSpan: 3
+          }
+        }
+      }
+      return {
+        children: text,
+          props: {
+            rowSpan: 0
+          }
+      }
+    }
   },
   {
-    title: '年龄',
-    dataIndex: 'age',
-    key: 'age',
-    width: 200,
-  },
-  {
-    title: '住址',
-    dataIndex: 'address',
-    key: 'address',
-    width: 200,
+    title: '类型',
+    dataIndex: 'type',
+    key: 'type',
+    width: 220,
+    children: [
+      {
+        title: '年龄',
+        dataIndex: 'age',
+        key: 'age',
+        width: 100,
+      },
+      {
+        title: '住址',
+        dataIndex: 'address',
+        key: 'address',
+        width: 120,
+      },
+    ]
   },
   {
     title: '生意',
@@ -50,22 +74,46 @@ const MyName:React.FC<IProps> = memo((props):React.ReactElement => {
 
   const [columns, setColumns] = useState<any[]>(initColumns)
 
+  const [selectedRowKeys, setSelectedRowKeys] = useState<any[]>([])
+
   const dataSource = [
     {
       key: '1',
       name: '胡彦斌',
       age: 32,
       address: '西湖区湖底公园1号',
-      date: '2020-01-01'
+      date: '2020-01-01',
+      pra: 3,
+      orid: 1,
     },
     {
       key: '2',
       name: '胡彦斌',
       age: 32,
       address: '西湖区湖底公园1号',
-      date: '2020-01-01'
+      date: '2020-01-01',
+      orid: 1,
+    },
+    {
+      key: '3',
+      name: '胡彦斌',
+      age: 32,
+      address: '西湖区湖底公园1号',
+      date: '2020-01-01',
+      orid: 1
     }
   ]
+
+  const rowSelection = {
+    getCheckboxProps: (record) => {
+      return {
+        style: {
+          display: record.pra ? '' : 'none'
+        }
+      }
+    },
+    selectedRowKeys: selectedRowKeys
+  }
 
   const handleResize = (nextColumns, column) => {
     // console.log(column)
@@ -78,9 +126,16 @@ const MyName:React.FC<IProps> = memo((props):React.ReactElement => {
         dataSource={dataSource}
         columns={columns}
         handleResize={handleResize}
+        rowSelection={rowSelection}
         // selectType="none"
         pagePosition={['bottomRight']}
-        onSelect={(selectedRows, selectedRowKeys) => {console.log(selectedRows)}}
+        onSelect={(selectedRows, selectedRowKeys) => {
+          let data = dataSource.filter(item => {
+            return selectedRows[0] && selectedRows[0]['orid'] === item.orid
+          }).map(item => item.key)
+          console.log(selectedRowKeys)
+          setSelectedRowKeys(data)
+        }}
       />
     </div>
   )
